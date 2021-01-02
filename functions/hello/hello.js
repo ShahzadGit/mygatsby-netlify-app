bb78742b868c8b2c1cd13ae65191a3cdab49ccf6
Shahzad Ahmed <shahzad.btm@gmail.com>
2021-01-02 10:57:27 +0500

    Added library in hello function

--- functions/hello/hello.js
+++ functions/hello/hello.js
@@ -1,5 +1,26 @@
+var random = require('random-name');
+
+exports.handler = async (event, context) => {
+  try {
+    console.log(random());
+    let name = random.first();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${name}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (err) {
+    return { statusCode: 500, body: err.toString() }
+  }
+}
+
+
+
+
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
+/* const handler = async (event) => {
   try {
     const subject = event.queryStringParameters.name || 'EveryOne'
     return {
@@ -15,3 +36,5 @@ const handler = async (event) => {
 }
 
 module.exports = { handler }
+
+*/
